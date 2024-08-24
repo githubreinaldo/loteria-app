eb15351c92795cf2cd2d0ebeef5579bee86f2dfc
githubreinaldo <reinaldobermudezcr@gmail.com>
2024-08-24 19:30:56 -0300

    Sincronizando cambios locales con GitHub

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 
 // Escuchar en el puerto 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
