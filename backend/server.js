a8ba8ff581aa8a828b05e935899e7aaff3b34e51
githubreinaldo <reinaldobermudezcr@gmail.com>
2024-08-25 23:44:47 -0300

    Crear public/index.html y modificar server.js para servir el frontend

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,15 @@ app.use(express.json());
 // Usar las rutas de la API
 app.use('/api', apiRoutes);
 
-// Servir archivos estáticos del frontend
-app.use(express.static(path.join(__dirname, '../src')));
+// Servir archivos estáticos desde public
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../src', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
 // Escuchar en el puerto 3001
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
