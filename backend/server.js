2558b44c7bd58d8fa1f895a4ae0b33096e2c6cf5
githubreinaldo <reinaldobermudezcr@gmail.com>
2024-08-25 23:41:00 -0300

    Configurar server para servir frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const connectDB = require('./config/database');  // Conectar con MongoDB
 const apiRoutes = require('./routes/api');
@@ -14,8 +15,15 @@ app.use(express.json());
 // Usar las rutas de la API
 app.use('/api', apiRoutes);
 
+// Servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, '../src')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../src', 'index.html'));
+});
+
 // Escuchar en el puerto 3001
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
