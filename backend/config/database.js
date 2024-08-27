f40e9f613b37ddd8feb5162542b17e0eaa49a67d
githubreinaldo <reinaldobermudezcr@gmail.com>
2024-08-26 21:28:56 -0300

    Actualización de archivos

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Conectar a MongoDB Atlas con la URI proporcionada
-    const conn = await mongoose.connect('mongodb+srv://reinaldobermudezcr:HU6teuSlMiovL2Gh@loteria.dr7wa.mongodb.net/?retryWrites=true&w=majority&appName=Loteria');
+    // Asegúrate de que el nombre de la base de datos sea 'Loteria' en la URI
+    const conn = await mongoose.connect('mongodb+srv://reinaldobermudezcr:HU6teuSlMiovL2Gh@loteria.dr7wa.mongodb.net/Loteria?retryWrites=true&w=majority&appName=Loteria');
     console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
