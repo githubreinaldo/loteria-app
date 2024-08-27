c4b7303f270b9d564de39bc0e22e1b7a13ef93f1
githubreinaldo <reinaldobermudezcr@gmail.com>
2024-08-26 22:05:57 -0300

    Actualización de archivos

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -12,17 +12,21 @@ const PlayScreen = () => {
     }
 
     try {
-      const response = await fetch('/api/play', {
+      const response = await fetch('https://7jtlss-3001.csb.app/play', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ number: playNumber, amount: betAmount }),
+        body: JSON.stringify({
+          username: 'nuevoUsuario',  // Incluye el nombre de usuario
+          numberPlayed: playNumber,
+          amount: betAmount
+        }),
       });
 
       const data = await response.json();
 
-      if (data.success) {
+      if (data.message === 'Jugada registrada') {
         setMessage('¡Jugada realizada con éxito!');
       } else {
         setMessage('Error al realizar la jugada.');
